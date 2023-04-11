3b609d251fdb8bec5148c3534b573acf45791846
Thomas <tjb47.dev@gmail.com>
2023-04-11 14:03:48 -0400

    completed prompts for user input

--- index.js
+++ index.js
@@ -11,10 +11,54 @@ function init() {
     {
         type: 'input',
         name: 'title',
-        message: "1. What's the title of your project?",
+        message: "What is the title of your project?",
     },
-
-
+    {
+        type: 'input',
+        name: 'description',
+        message: "Provide a description of your project?",
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "How do you install your project?",
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What is the usage of this project?",
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Do you have a Choice of License?",
+        choices: ['MIT License', 'Apache License 2.0', 'GNU GPLv3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "Where there any contributors to this project?",
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "Any tests examples?",
+    },     
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please provide your github.",
+    },     
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please provide your email.",
+    },     
+    {
+        type: 'input',
+        name: 'questions',
+        message: "Please provide your email.",
+    }, 
 ])
 }
 
