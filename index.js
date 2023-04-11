8d8587db58795b6218daad5f70e7723fe9958bd3
Thomas <tjb47.dev@gmail.com>
2023-04-11 13:50:38 -0400

    created a function to initialize the app and prompt user with questions

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to initialize app
+function init() {
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: "1. What's the title of your project?",
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+
+])
+}
 
 // Function call to initialize app
 init();
